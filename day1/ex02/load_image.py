64fa8843ba2f9a9eecac7d741168063d2bb325a5
Meryem COURTOIS <mcourtoi@paul-f4Br9s3.clusters.42paris.fr>
2025-01-13 00:04:45 +0100

    dep.txt

--- day1/ex02/load_image.py
+++ day1/ex02/load_image.py
@@ -2,18 +2,28 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-im = Image.open("animal.jpeg")
+im = Image.open("landscape.jpg")
 
 # im.show()
-pxarr = np.asarray(im)
-print(f'The shape of the image is {pxarr.shape}')
+pxarr = np.array(im)
+print(f"The shape of the image is {pxarr.shape}")
 print(im.format)
-print(pxarr)
+# print(pxarr)
 
-newarr = pxarr[184:584,312:712]
-print(f'The new shape of the image is {newarr.shape}')
-newim = Image.fromarray(newarr)
+# tarray = np.array([x for x in pxarr[0,:,:]], [x for x in pxarr[:,0,:]])
+height, width, channels = pxarr.shape
+tarray = np.zeros((width, height, channels), dtype=pxarr.dtype)
+
+
+for i in range(width):
+    tarray[i] = pxarr[:, i]
+
+
+# newarr = pxarr[184:584, 312:712]
+# print(f"The new shape of the image is {newarr.shape}")
+# newim = Image.fromarray(newarr)
 # newim.show()
 
-plt.imshow(newarr)
+invert = 255 - pxarr
+plt.imshow(invert)
 plt.show()
