c478d35124b115198c165be47a455a5bceff2f74
Meryem COURTOIS <mcourtoi@paul-f4Br8s1.clusters.42paris.fr>
2025-01-12 17:36:33 +0100

    need to rotate matrix

--- day1/ex02/load_image.py
+++ day1/ex02/load_image.py
@@ -1,3 +1,19 @@
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
-im = Image.open("../")
+im = Image.open("animal.jpeg")
+
+# im.show()
+pxarr = np.asarray(im)
+print(f'The shape of the image is {pxarr.shape}')
+print(im.format)
+print(pxarr)
+
+newarr = pxarr[184:584,312:712]
+print(f'The new shape of the image is {newarr.shape}')
+newim = Image.fromarray(newarr)
+# newim.show()
+
+plt.imshow(newarr)
+plt.show()
